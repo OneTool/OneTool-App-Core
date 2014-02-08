ec90b1505a6af418a8521d2e57e51c9488c67d75
Sebastien Thebert <sebthebert@gmail.com>
2014-02-08 01:18:16 +0100

    Initial commit

---
+++ lib/OneTool/App.pm
@@ -0,0 +1,30 @@
+
+=head1 NAME
+
+OneTool::App - Module for any OneTool onetool_*.pl program
+
+=head1 DESCRIPTION
+
+It defines default options for any onetool_*.pl program
+
+Deafult options are:
+  -D/--debug
+  -h/--help
+  -v/--version
+  
+=cut
+
+package OneTool::App;
+
+use strict;
+use warnings;
+
+our @DEFAULT_OPTIONS = ('debug|D', 'help|h', 'version|v',);
+
+1;
+
+=head1 AUTHOR
+
+Sebastien Thebert <contact@onetool.pm>
+
+=cut
